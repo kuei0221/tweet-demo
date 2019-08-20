3ed4ca8a06f4b956e95e95ab2843730f49d0a7fe
kuei0221 <michaelhwang0619@gmail>
2019-08-20 16:15:41 +0800

    add static_pages routes/controller/view, add bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title)
+    default = "tweet-demo"
+    unless page_title.nil?
+      "#{default}: #{page_title}"
+    else
+      default
+    end
+  end
 end
