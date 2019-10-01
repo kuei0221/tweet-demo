6fc8980f78f1060710b49cffe7b66e78a4ae67c2
kuei0221 <michaelhwang0619@gmail>
2019-10-01 22:03:35 +0800

    Add clipboard.js to allow link button works. Setting is coded inside application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,26 @@
 //= require popper
 //= require bootstrap-sprockets
 //= require activestorage
+//= require clipboard.min
 //= require turbolinks
 //= require_tree .
+
+$(document).on('turbolinks:load', function(){
+  var clipboard = new ClipboardJS('.clipboard');
+
+  clipboard.on('success', function(e) {
+    console.info('Action:', e.action);
+    console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
+  
+    // e.clearSelection();
+  });
+  
+  clipboard.on('error', function(e) {
+    console.error('Action:', e.action);
+    console.error('Trigger:', e.trigger);
+  });
+
+});
 
 
